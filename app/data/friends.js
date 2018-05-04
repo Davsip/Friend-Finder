54c05df2f07943ce8be03719b33cb74538bdf0a5
davsip <david.sipala@gmail.com>
2018-05-03 23:51:43 -0500

    last update

---
+++ app/data/friends.js
@@ -0,0 +1,154 @@
+//This is the DATA of friends
+
+var friends = [
+    {
+      name: "John Stewart",
+      photo: "(Photo location??)",
+      scores: [
+        "5",
+        "1",
+        "3",
+        "5",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Sean Pena",
+      photo: "(Photo location??)",
+      scores: [
+        "5",
+        "1",
+        "3",
+        "5",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Ed Moyer",
+      photo: "(Photo location??)",
+      scores: [
+        "5",
+        "1",
+        "3",
+        "5",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Willy Ballena",
+      photo: "(Photo location??)",
+      scores: [
+        "5",
+        "1",
+        "3",
+        "5",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Phil Colilla",
+      photo: "(Photo location??)",
+      scores: [
+        "5",
+        "1",
+        "3",
+        "5",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Pepe Rompé",
+      photo: "(Photo location??)",
+      scores: [
+        "5",
+        "1",
+        "3",
+        "5",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Mirko Venice",
+      photo: "(Photo location??)",
+      scores: [
+        "5",
+        "1",
+        "3",
+        "6",
+        "6",
+        "1",
+        "6",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Benito Gialo",
+      photo: "(Photo location??)",
+      scores: [
+        "3",
+        "1",
+        "3",
+        "5",
+        "5",
+        "3",
+        "2",
+        "3",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Juan Bryan",
+      photo: "(Photo location??)",
+      scores: [
+        "5",
+        "1",
+        "3",
+        "5",
+        "7",
+        "7",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    }
+  
+  ];
+  //Export the array. This makes it availible by using require.
+  
+  module.exports = friends;
+  
+
